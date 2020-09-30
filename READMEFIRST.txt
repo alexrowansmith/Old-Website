29e35480c52d2238741bb2dddf2be1d1677694ad
alexrowansmith <56225654+alexrowansmith@users.noreply.github.com>
2020-09-30 18:26:52 +0100

    new website
    pushed new website,removed old one.

--- READMEFIRST.txt
+++
@@ -1,1 +0,0 @@
-Please right-click on the .zip file and "extract all" to a new folder. To open the webpage, open about-me.html inside the extracted folder.
---
+++ js.js
@@ -0,0 +1,61 @@
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+                function myFunction1() {
+                  var dots1 = document.getElementById("dots1");
+                  var moreText1 = document.getElementById("more1");
+                  var btnText1 = document.getElementById("myBtn1");
+
+                  if (dots1.style.display === "none") {
+                    dots1.style.display = "inline";
+                    btnText1.innerHTML = "Read more";
+                    moreText1.style.display = "none";
+                  } else {
+                    dots1.style.display = "none";
+                    btnText1.innerHTML = "Read less";
+                    moreText1.style.display = "inline";
+                  }
+                }
+                function myFunction2() {
+                  var dots2 = document.getElementById("dots2");
+                  var moreText2 = document.getElementById("more2");
+                  var btnText2 = document.getElementById("myBtn2");
+
+                  if (dots2.style.display === "none") {
+                    dots2.style.display = "inline";
+                    btnText2.innerHTML = "Read more";
+                    moreText2.style.display = "none";
+                  } else {
+                    dots2.style.display = "none";
+                    btnText2.innerHTML = "Read less";
+                    moreText2.style.display = "inline";
+                  }
+                }
+                function myFunction3() {
+                  var dots3 = document.getElementById("dots3");
+                  var moreText3 = document.getElementById("more3");
+                  var btnText3 = document.getElementById("myBtn3");
+
+                  if (dots3.style.display === "none") {
+                    dots3.style.display = "inline";
+                    btnText3.innerHTML = "Read more";
+                    moreText3.style.display = "none";
+                  } else {
+                    dots3.style.display = "none";
+                    btnText3.innerHTML = "Read less";
+                    moreText3.style.display = "inline";
+                  }
+                }
